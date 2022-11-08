12b1d74958cfd2ab1ffb15d3bc9cb2037c9d0f3f
Laila AlKandery <lailaabdulraheem@gmail.com>
2022-11-08 16:55:44 +0300

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,10 @@
+void main() {
+  final movie = {
+    'name': 'The Amazing SpiderMan',
+    'genre': 'Action',
+    'ratings': [1.5,3.2,6.4]
+  };
+  print(movie['name']);
+  print(movie['genre']);
+  print(movie['ratings']);
+}
